62179b6757a8be6394fed24bd839a29f8c549e5f
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-07-29 20:25:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+function getElement(){
+	return new Promise(resolve=>{
+		let arr=[1,2,3,4]
+		resolve(arr);
+	})
+}
+function evenNum(arr){
+	return new Promise(resolve=>{
+		setTimeout(()=>{
+			let filteredArr=arr.filter(num=>num%2==0);
+			document.getElementById('output').textContent=`Even Numbers are= ${filteredArr}`
+			resolve(filteredArr);
+		},1000);		
+	})
+}
+function multiplyEven(numbers){
+	return new Promise(resolve=>{
+		setTimeout(()=>{
+			let mulArr=numbers.map(num=>num*2);
+			document.getElementById('output').textContent=`Multiply Numbers are = ${mulArr}`
+			resolve(mulArr);
+		},2000);		
+	})
+}
+getElement()
+.then(evenNum)
+.then(multiplyEven)
